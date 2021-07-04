8e2ffa5af629413a244aa9fc5f41f463993d01c9
lasthopestars <58655507+lasthopestars@users.noreply.github.com>
2021-07-04 19:21:20 +0900

    Add files via upload

---
+++ Control flow.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cstdlib>
+
+
+using namespace std;
+
+int main()
+{
+	std::cout<<"I love you"<<std::endl;
+	
+	exit(0);
+	
+	std::cout<<"I love you, too"<<std::endl;
+}
+
