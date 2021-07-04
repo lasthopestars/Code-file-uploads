b7873b52e3329462500d826d48813f280c18d256
lasthopestars <58655507+lasthopestars@users.noreply.github.com>
2021-07-04 20:23:45 +0900

    Add files via upload

---
+++ goto.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+
+
+
+int main()
+{
+	double x;
+
+tryAgain: //label
+	cout << "Enter a non-negative number" << endl;
+	cin >> x;
+
+	if (x < 0.0)
+		goto tryAgain;
+
+	cout<<sqrt(x)<<endl;
+	return 0;
+}
