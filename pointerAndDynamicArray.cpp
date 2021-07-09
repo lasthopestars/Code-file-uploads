9c30aeae9cbb34b095f1d2fe23a79c3c2f0327af
lasthopestars <58655507+lasthopestars@users.noreply.github.com>
2021-07-09 21:18:42 +0900

    Add files via upload

---
+++ pointerAndDynamicArray.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	const int row = 3;
+	const int col = 5;
+
+	const int s2da[][5]=
+	{
+		{1,2,3,4,5},
+		{6,7,8,9,10},
+		{11,12,13,14,15}
+	};
+
+	
+
+	int *matrix = new int[row*col];
+
+	for (int r = 0; r < row; ++r)
+		for (int c = 0; c < col; ++c)
+			matrix[c + col * r] = s2da[r][c];
+
+
+
+	for (int r = 0; r < row; ++r)
+	{
+		for (int c = 0; c < col; ++c)
+			cout << matrix[c+col*r] << " ";
+		cout << endl;
+	}
+
+
+	
+	
+	
+	delete[] matrix;
+
+
+	/*int **matrix = new int*[row];
+
+	for(int r=0;r<row;++r)
+		//matrix[r]= new int[col];
+
+	for (int r = 0; r < row; ++r)
+	{
+		for (int c = 0; c < col; ++c)
+			matrix[r][c] = s2da[r][c];
+		
+	}
+
+	for (int r = 0; r < row; ++r)
+	{
+		for (int c = 0; c < col; ++c)
+			cout << matrix[r][c] << " ";
+		cout << endl;
+	}
+		
+	for (int r = 0; r < row; ++r)
+		delete[] matrix[r];
+
+	delete[] matrix;
+
+
+	return 0;
+	*/
+}
