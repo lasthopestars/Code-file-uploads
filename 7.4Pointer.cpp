8dcdc0165f305bb8ae3fe089bb9187017c6b96f9
lasthopestars <58655507+lasthopestars@users.noreply.github.com>
2021-07-10 22:27:13 +0900

    Add files via upload

---
+++ 7.4Pointer.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+typedef int* pint;
+
+void foo(double degrees, double *sin_out, double *cos_out)
+{
+	*sin_out = 1.0;
+	*cos_out = 2.0;
+
+
+}
+
+int main()
+{
+	int value = 5;
+
+	cout << value << " " << &value << endl;
+	
+	int *ptr = &value;
+
+	cout << &ptr << endl;
+	
+
+	double degrees = 30;
+	double sin, cos;
+
+	foo(degrees, &sin, &cos);
+
+	cout << sin << " " << cos << endl;
+	
+
+	return 0;
+}
