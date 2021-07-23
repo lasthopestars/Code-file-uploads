32f59212cd45c5e88a9069c8c1882389a7480b25
lasthopestars <58655507+lasthopestars@users.noreply.github.com>
2021-07-23 13:36:55 +0900

    Add files via upload

---
+++ IncrementOperatorOverloading.cpp
@@ -0,0 +1,66 @@
+
+#include<iostream>
+using namespace std;
+
+
+class Digit
+{
+private:
+	int m_digit;
+
+public:
+	Digit(int digit=0) : m_digit(digit){}
+
+
+	//implementing prefix method
+
+	Digit & operator++()
+	{
+		++m_digit;
+
+		return *this;
+
+	}
+
+
+	//implementing postfix method
+
+	Digit operator++(int)//the postfix operator needs sth inside(such as int)
+	{
+		Digit temp(m_digit);
+
+		++(*this);
+
+		return temp;
+
+	}
+
+
+
+
+
+	friend ostream & operator<< (ostream &out, const Digit &d)
+	{
+		out << d.m_digit;
+		return out;
+	}
+};
+
+
+
+
+
+
+
+
+int main()
+{
+	Digit d(5);
+	cout << ++d << endl;
+	cout << d << endl;
+
+	cout << d++ << endl;
+	cout << d << endl;
+	
+		return 0;
+}
