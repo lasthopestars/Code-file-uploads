c426d576dc41065165a8291ac32eddb6cc8f8013
lasthopestars <58655507+lasthopestars@users.noreply.github.com>
2021-08-05 13:50:07 +0900

    Add files via upload

---
+++ 13.1Cents.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include "Cents.h"
+
+template<typename T>
+
+
+T getMax(T x, T  y)
+{
+	return (x > y) ? x : y;
+}
+
+//다 지우고 이거 하나만 남김. float, char 등. 아주 깔끔.
+
+int main()
+{
+	std::cout << getMax(1, 2) << std::endl;
+	std::cout << getMax(3.14, 1.592) << std::endl;
+	std::cout << getMax(1.0f, 3.4f) << std::endl;
+	std::cout << getMax('a', 'c') << std::endl;
+
+	std::cout << getMax(Cents(5), Cents(9)) << std::endl;
+
+
+	return 0;
+}
